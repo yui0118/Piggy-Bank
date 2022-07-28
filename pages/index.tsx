801abcdac88d0cc1af9d5d9b59f0f23e6feaf8e6
yui0118 <yui.orange.hello@gmail.com>
2022-07-29 01:15:55 +0900

    Add: 目標を保存するstateであるgoalsの数によって、コンポーネントで表示分けをする

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,26 +4,21 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import { useState } from "react";
+import Goals from "../components/goals";
+import Welcome from "../components/welcome";
+import { Modal } from "../components/modal";
 
 const Home: NextPage = () => {
-  const [havingGoals, setHavingGoals] = useState(false);
+  const [goals, setGoals] = useState<string[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
   return (
     <>
-      {!havingGoals ? (
-        <>
-          <h1>自己投資貯金箱</h1>
-          <p>
-            自己投資貯金箱は、「達成したい目標に対していくら投資したのか」を可視化できるサービスです！
-          </p>
-          {/* 豚の貯金箱の画像を挿入 */}
-          <p>早速目標を入力して、自己投資貯金を始めましょう!!</p>
-          <button onClick={() => setHavingGoals(true)}>目標を入力</button>
-        </>
+      {goals.length > 0 ? <Goals goals={goals} /> : <Welcome />}
+      {isModalOpen ? (
+        <Modal onAddGoals={(goal: string) => setGoals([...goals, goal])} />
       ) : (
-        <>
-          <h1>目標一覧</h1>
-          <button onClick={() => setHavingGoals(false)}>トップページへ</button>
-        </>
+        <button>目標を入力</button>
       )}
     </>
   );
